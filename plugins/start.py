69de474a1d86388fb1da5bb427d57ef03f533529
MagicAngelTG <88583278+MagicAngelTG@users.noreply.github.com>
2021-08-07 20:00:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,8 +10,7 @@ async def start(client, message):
          ],
          [
          InlineKeyboardButton("Developer", url="https://t.me/Akfronic"),
-         InlineKeyboardButton("Share", url="https://t.me/share/url?url=Join%20%40X_Botz")
-    ])
+         InlineKeyboardButton("Share", url="https://t.me/share/url?url=Join%20%40X_Botz")]])
     welcomed = f"Hai <b>{message.from_user.mention}, \n\n**Iam a Youtube Video Downloader Bot. Send Me Any YouTube Video Link I Will Upload To Telegram With Custum Thumbnail Support\n\nFor More Details Press** /help\n\n**⚜️ Maintained By : @X_Botz**"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
