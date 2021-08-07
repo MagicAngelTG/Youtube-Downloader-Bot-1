f1cee2c05a6c276b1181de17113e683e7ab8d964
MagicAngelTG <88583278+MagicAngelTG@users.noreply.github.com>
2021-08-07 19:54:16 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,6 +3,6 @@ from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(Filters.command(["help"]))
 async def help(client, message):
-    helptxt = f"Are You Confused With Me 🤣. \nJust Follow Up Below Instructions 😉.\n\n**➲ Send Me A YouTube Video Link\n➲ Wait A Bit For Analysis Your Link \n➲ Select Any Quality Button \n➲ Then Select Upload Mode \n➲ Wait Few Seconds...I Will Upload It To Telegram.**\n\n\nJoin @EKBOTZ_UPDATE FOR KNOW MORE."
+    helptxt = f"Are You Confused With Me 🤣. \nJust Follow Up Below Instructions 😉.\n\n**➲ Send Me A YouTube Video Link\n➲ Wait A Bit For Analysis Your Link \n➲ Select Any Quality Button \n➲ Then Select Upload Mode \n➲ Wait Few Seconds...I Will Upload It To Telegram.**\n\n**⚜️ Maintained By : @X_Botz**"
     helpbtn = InlineKeyboardMarkup([[InlineKeyboardButton("Channel ⭕️", url ="https://t.me/EKBOTZ_UPDATE")]])
     await message.reply_text(helptxt, reply_markup = helpbtn)
